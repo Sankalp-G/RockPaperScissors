8eab3c89bb139b839bbfdd75133cd883f2540fb2
Sankalp-G <sankalp.gawali@gmail.com>
2021-05-30 17:04:28 +0530

    made transition for buttons and subtitles

--- scripts.js
+++ scripts.js
@@ -113,3 +113,84 @@ menu_buttons.forEach((button) => {
         }
     });
 });
+
+
+const button_container = document.querySelector('#button_container')
+const menu_button_container = document.querySelector('#menu_button_container')
+const menu_button_img = document.querySelectorAll('#menu_button_img')
+const subtitle_container = document.querySelector('#subtitle_container')
+
+const game_button_container = document.createElement('div')
+const rock_button = document.createElement('div')
+const paper_button = document.createElement('div')
+const scissor_button = document.createElement('div')
+const rock_img = document.createElement('img')
+const paper_img = document.createElement('img')
+const scissor_img = document.createElement('img')
+
+rock_img.setAttribute("id", "game_button_img")
+rock_img.setAttribute("src", "assets/icons/rock_icon.png")
+paper_img.setAttribute("id", "game_button_img")
+paper_img.setAttribute("src", "assets/icons/paper_icon.png")
+paper_img.setAttribute("style", "height: 220px; width: 220px; padding: 16px;")
+scissor_img.setAttribute("id", "game_button_img")
+scissor_img.setAttribute("src", "assets/icons/scissor_icon.png")
+scissor_img.setAttribute("style", "height: 210px; width: 210px; padding: 21px;")
+
+rock_button.setAttribute("id", "button2")
+rock_button.setAttribute("style", "background-color: #ff5460;")
+paper_button.setAttribute("id", "button2")
+paper_button.setAttribute("style", "background-color: #85a3cd;")
+scissor_button.setAttribute("id", "button2")
+scissor_button.setAttribute("style", "background-color: #a7e164;")
+game_button_container.setAttribute("id", "game_button_container")
+
+game_button_container.appendChild(rock_button)
+game_button_container.appendChild(paper_button)
+game_button_container.appendChild(scissor_button)
+rock_button.appendChild(rock_img)
+paper_button.appendChild(paper_img)
+scissor_button.appendChild(scissor_img)
+
+//button_container.appendChild(game_button_container)
+
+
+function gameMode(){
+    button_container.classList.add('button_container2');
+
+    let booper = true;
+
+    menu_button_img.forEach((button) => {
+
+
+        button.classList.add('menu_button_img2');
+          
+        button.addEventListener("transitionend", () =>{
+            if(button_container.firstElementChild == menu_button_container && booper === true){
+                button_container.removeChild(menu_button_container)
+                button_container.appendChild(game_button_container)
+                booper = false;
+                }
+        });
+    });
+    subtitle_container.setAttribute("style", "opacity: 0;")
+
+}
+
+ function menuMode(){
+
+
+    if(button_container.firstElementChild !== menu_button_container){
+        button_container.appendChild(menu_button_container)
+        button_container.removeChild(game_button_container)
+        }
+
+        menu_button_img.forEach((button) => {
+            button.classList.remove('menu_button_img2');
+        });
+
+    button_container.classList.remove('button_container2');
+
+    subtitle_container.setAttribute("style", "opacity: 100; transition: 1s;")
+
+}
